9cc0311860a135ed71a5e421af9243fd2cb4a2a2
T W <ymir@ulthar.xyz>
2021-06-16 16:56:35 -0700

    Moved book printing to a seperate function

--- audible-dl.go
+++ audible-dl.go
@@ -16,27 +16,10 @@ import (
 //
 func main() {
 
-	books, err := GetAllBooks()
+	_, err := GetAllBooks()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		fmt.Fprintf(os.Stderr, "I wasn't able to scrape you library :(")
 		os.Exit(1)
-	}
-
-	for _, b := range books {
-		fmt.Println("==================================================")
-		fmt.Println("SLUG:        ", b.Slug)
-		fmt.Println("TITLE:       ", b.Title)
-		fmt.Println("SERIES:      ", b.Series)
-		fmt.Println("NUMBER:      ", b.SeriesIndex)
-		fmt.Println("RUNTIME:     ", b.Runtime)
-		fmt.Println("SUMMARY:     ", b.Summary)
-		fmt.Println("AUTHOR(S):   ", b.Authors)
-		fmt.Println("NARRATOR(S): ", b.Narrators)
-		fmt.Println("--------------------------------------------------")
-		fmt.Println("COVER URL:   ", b.CoverURL)
-		fmt.Println("DOWNLOAD URL:", b.DownloadURL)
-		fmt.Println("RESOURCE URL:", b.CompanionURL)
-		fmt.Println("==================================================")
 	}
 }
