5748eeff5f6ba0a601d8462ed57b099f17e9c7d8
ꙮ <ymir@ulthar.xyz>
2021-06-26 20:04:14 -0700

    It works!
    - Added initial logic for the tmp directories
    - Added nice printouts
    - Added cracker function

--- audible-dl.go
+++ audible-dl.go
@@ -9,6 +9,7 @@ package main
 import (
 	"os"
 	"log"
+	"fmt"
 )
 
 // Later we'll generate a list of the ones we don't have
@@ -21,20 +22,19 @@ func getFarsideBooks(far []Book) []Book {
 //
 func main() {
 
-	err := os.Mkdir(".audible-dl-downloading", 0755)
-	if err != nil {
+	if err := os.Mkdir(".audible-dl-downloading", 0755); err != nil {
 		if err.(*os.PathError).Err.Error() == "file exists" {
 			log.Fatal("TODO: attempt to recover from error")
 		}
 	}
 
-	err := os.Mkdir(".audible-dl-converting", 0755)
-	if err != nil {
+	if err := os.Mkdir(".audible-dl-converting", 0755); err != nil {
 		if err.(*os.PathError).Err.Error() == "file exists" {
 			log.Fatal("TODO: attempt to recover from error")
 		}
 	}
 
+	fmt.Println("\033[1mScraping Library:\033[m")
 	books, err := GetAllBooks()
 	if err != nil {
 		log.Fatal(err)
@@ -42,17 +42,25 @@ func main() {
 
 	latest := getFarsideBooks(books)
 
+	fmt.Println("\033[1mDownloading Books:\033[m")
 	for i := 0; i < 1; i++ {
-		err := DownloadBook(latest[i])
-		if err != nil {
-			log.Fatal(err)
+		if err := DownloadBook(latest[i]); err != nil {
+			log.Print(err)
 		}
 	}
 
+	fmt.Println("\033[1mConverting Books:\033[m")
 	for i := 0; i < 1; i++ {
-		err := CrackAAX(latest[i])
-		if err != nil {
-			log.Fatal(err)
+		if err := CrackAAX(latest[i].FileName); err != nil {
+			log.Print(err)
 		}
+	}
+
+	if err = os.Remove(".audible-dl-downloading"); err != nil {
+		log.Print("Some download operations failed.")
+	}
+
+	if err = os.Remove(".audible-dl-converting"); err != nil {
+		log.Print("Some conversion operations failed.")
 	}
 }
