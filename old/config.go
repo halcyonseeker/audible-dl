24e2a253e427e893eb341aa2b4dc1e03537cae29
T W <ymir@ulthar.xyz>
2021-09-25 18:24:52 -0700

    Removed old code

--- old/config.go
+++
@@ -1,7 +0,0 @@
-package main
-
-// Store general runtime data. For now we only have they bytes field
-type Config struct {
-	// You Audible activation bytes, required to decrypt book
-	Bytes string
-}
