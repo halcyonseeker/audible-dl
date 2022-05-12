4b6b689f0e0d929df30736a4cd60bec3808b998e
T W <ymir@ulthar.xyz>
2022-05-12 11:34:47 -0700

    moved scraper into the new file
    The scraper isn't quite working, I'm not sure if this is an error on
    my end or a change to the damn html.
    
    I should probably add a debugging flag to print the status of the
    webscraper...

--- audible-dl.go
+++ audible-dl.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
+	"golang.org/x/net/html"
 	"gopkg.in/yaml.v2"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
+	"net/url"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
+	"strconv"
 	"strings"
+	"sync"
 )
 
 ////////////////////////////////////////////////////////////////////////
@@ -120,6 +127,333 @@ func (a *Account) Convert(in, out string, client Client) (error, []byte) {
 		return err, nil
 	}
 	return nil, nil
+}
+
+// Download a HTMl page in the user's library
+func (a *Account) getLibraryPage(page int) ([]byte, error) {
+	jar, _ := cookiejar.New(nil)
+	client := &http.Client{Jar: jar}
+	uri := "https://www.audible.com/library/titles?page=" + strconv.Itoa(page)
+	req, _ := http.NewRequest("GET", uri, nil)
+
+	jaruri, _ := url.ParseRequestURI(uri)
+	jar.SetCookies(jaruri, a.Auth)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New("getLibraryPage: " + resp.Status)
+	}
+
+	html, _ := ioutil.ReadAll(resp.Body)
+	return html, nil
+}
+
+// Scrape the library until we encounter a book whose filename
+// (display title ran through stripstr) matches lim, returning a slice
+// of books.  If lim is an empty string this behaves exactly like
+// ScrapeFullLibrary().
+func (a *Account) ScrapeLibraryUntil(pagenum chan int, lim string) ([]Book, error) {
+	var books []Book
+
+	defer close(pagenum)
+
+	// audible.com/library/titles?page=N doesn't return a 404 when
+	// we access pages that don't exist, so we'll store the slug of
+	// the first books of the current and previous pages in these
+	// respective variables and check against the current book's slug.
+	var firstincurrpage string = ""
+	var firstinprevpage string = ""
+
+	for i := 1; ; i++ {
+		pagenum <- i
+		raw, err := a.getLibraryPage(i)
+		if err != nil {
+			return nil, err
+		}
+
+		dom := html.NewTokenizer(bytes.NewReader(raw))
+
+		for {
+			tt := dom.Next()
+			tok := dom.Token()
+			if tokBeginsBook(tt, tok) {
+				// If we find a book, extract it
+				book := xSingleBook(dom, tt, tok)
+				if book.Slug == firstinprevpage {
+					// We've reached a duplicate page
+					return books, nil
+				}
+				if book.FileName == lim && lim != "" {
+					// We've reached the final book
+					return books, nil
+				}
+				books = append(books, book)
+				if firstincurrpage == "" {
+					// Save the first book in the page
+					firstincurrpage = book.Slug
+				}
+				if firstinprevpage == "" {
+					// This is the first page
+					firstinprevpage = book.Slug
+				}
+				continue
+			}
+
+			// exit inner loop when we reach the end end
+			if id(tok) == "center-6" || tt == html.ErrorToken {
+				break
+			}
+		}
+		// We're fetching the next page, so we cycle these out
+		firstinprevpage = firstincurrpage
+		firstincurrpage = ""
+
+		// If we didn't extract any books from the first page then
+		// we probably won't extract any from the next, so we should
+		// just break and return an error, saving the page source in
+		// a file along with debugging information
+		if len(books) == 0 {
+			ioutil.WriteFile(".audible-dl-debug.html", raw, 0644)
+			return nil, errors.New("Failed to extract books from HTML")
+		}
+	}
+}
+
+func (a *Account) ScrapeFullLibrary(pagenum chan int) ([]Book, error) {
+	return a.ScrapeLibraryUntil(pagenum, "")
+}
+
+////////////////////////////////////////////////////////////////////////
+//                                             _   _ _ _ _
+//  ___  ___ _ __ __ _ _ __   ___ _ __   _   _| |_(_) (_) |_ ___  ___
+// / __|/ __| '__/ _` | '_ \ / _ \ '__| | | | | __| | | | __/ _ \/ __|
+// \__ \ (__| | | (_| | |_) |  __/ |    | |_| | |_| | | | ||  __/\__ \
+// |___/\___|_|  \__,_| .__/ \___|_|     \__,_|\__|_|_|_|\__\___||___/
+//                    |_|
+////////////////////////////////////////////////////////////////////////
+
+// Each book is stored in one of these
+type Book struct {
+	Slug         string   // B002VA9SWS
+	Title        string   // "The Hitchhiker's Guide to the Galaxy"
+	Series       string   // "The Hitchhiker's Guide to the Galaxy"
+	Runtime      string   // "5 hrs and 51 minutes"
+	Summary      string   // "Seconds before the Earth is demolished..."
+	CoverURL     string   // "https://m.media-amazon.com/..."
+	FileName     string   // "TheHitchhikersGuidetotheGalaxy"
+	DownloadURL  string   // "https://cds.audible.com/..."
+	CompanionURL string   // ""
+	Authors      []string // ["Douglas Adams"]
+	Narrators    []string // ["Steven Fry"]
+	SeriesIndex  int      // 1
+}
+
+// Determine if the current html token contains a book
+func tokBeginsBook(tt html.TokenType, tok html.Token) bool {
+	return tt == html.StartTagToken &&
+		class(tok) == "adbl-library-content-row"
+}
+
+// Remove whitespace and other shell reserve characters from S
+func stripstr(s string) string {
+	r := regexp.MustCompile(
+		`\s|\\|\(|\)|\[|\]|\{|\}|\*|\?|\!|\+|\,|\;|\&|\||\'|\"|‘`)
+	s = r.ReplaceAllString(s, "")
+
+	return s
+}
+
+// Remove extra whitespace and newlines from a string
+func cleanstr(s string) string {
+	r := regexp.MustCompile(`\s+`)
+	s = strings.TrimSpace(s)
+	s = strings.Trim(s, "\n")
+	s = r.ReplaceAllString(s, " ")
+
+	return s
+}
+
+// Get the class attribute of html tag T
+func class(t html.Token) string {
+	for _, a := range t.Attr {
+		if a.Key == "class" {
+			return cleanstr(a.Val)
+		}
+	}
+	return ""
+}
+
+// Get the href attribute of html tag T
+func href(t html.Token) string {
+	for _, a := range t.Attr {
+		if a.Key == "href" {
+			return cleanstr(a.Val)
+		}
+	}
+	return ""
+}
+
+// Get the id attribute of html tag T
+func id(t html.Token) string {
+	for _, a := range t.Attr {
+		if a.Key == "id" {
+			return cleanstr(a.Val)
+		}
+	}
+	return ""
+}
+
+// Get the aria-label (how is this different from id??) attriute of hmtl tag T
+func aria_label(t html.Token) string {
+	for _, a := range t.Attr {
+		if a.Key == "aria-label" {
+			return cleanstr(a.Val)
+		}
+	}
+	return ""
+}
+
+// Get the book's author and narrator (translator?)
+func xPeople(dom *html.Tokenizer) []string {
+	var people []string
+	var prevtag string
+	var pprevtag string
+	tt := dom.Next()
+	tok := dom.Token()
+
+	for tok.Data != "li" {
+		tt = dom.Next()
+		tok = dom.Token()
+
+		if tt == html.StartTagToken {
+			pprevtag = prevtag
+			prevtag = tok.Data
+		}
+
+		if tt == html.EndTagToken {
+			pprevtag = prevtag
+			prevtag = ""
+		}
+
+		if pprevtag == "a" && prevtag == "span" && tt == html.TextToken {
+			people = append(people, cleanstr(tok.Data))
+		}
+	}
+	return people
+}
+
+// Get the book's summary
+func xSummary(dom *html.Tokenizer, tt html.TokenType, tok html.Token) string {
+	var s string
+	for !(tt == html.EndTagToken && tok.Data == "span") {
+		tt = dom.Next()
+		tok = dom.Token()
+
+		if tt == html.TextToken {
+			s += " " + tok.Data + " "
+			s = cleanstr(s)
+		}
+	}
+	return s
+}
+
+// Get the book's series name and index number
+func xSeries(dom *html.Tokenizer, tt html.TokenType, tok html.Token) (string, int) {
+	var series string
+	var index int
+	for !(tt == html.EndTagToken && tok.Data == "span") {
+		tt = dom.Next()
+		tok = dom.Token()
+		if tt == html.TextToken {
+			if series == "" {
+				series = cleanstr(tok.Data)
+			} else if index == 0 {
+				s := cleanstr(tok.Data)
+				if strings.Contains(s, ", Book") {
+					index, _ = strconv.Atoi(s[len(s)-1:])
+				}
+				break
+			}
+		}
+	}
+	return series, index
+}
+
+// Fill in the structure for a single book
+func xSingleBook(dom *html.Tokenizer, tt html.TokenType, tok html.Token) Book {
+	var book Book
+
+	// First we'll extract the book's slug from its div's id
+	slug := id(tok)
+	book.Slug = slug[len(slug)-10:]
+
+	for {
+		tt = dom.Next()
+		tok = dom.Token()
+
+		if strings.Contains(class(tok), "bc-image-inset-border") {
+			////////// COVER IMAGE
+			for _, a := range tok.Attr {
+				if a.Key == "src" {
+					book.CoverURL = cleanstr(a.Val)
+				}
+			}
+		} else if class(tok) == "bc-text bc-size-headline3" {
+			////////// TITLE
+			tt = dom.Next()
+			tok = dom.Token()
+			book.Title = cleanstr(tok.Data)
+			continue
+		} else if strings.Contains(class(tok), "authorLabel") {
+			////////// AUTHORS
+			book.Authors = xPeople(dom)
+			continue
+		} else if strings.Contains(class(tok), "narratorLabel") {
+			////////// NARRATORS
+			book.Narrators = xPeople(dom)
+			continue
+		} else if strings.Contains(class(tok), "merchandisingSummary") {
+			////////// SUMMARY
+			book.Summary = xSummary(dom, tt, tok)
+			continue
+
+		} else if id(tok) == "time-remaining-display-"+book.Slug {
+			////////// RUNTIME
+			for !(tt == html.EndTagToken && tok.Data == "span") {
+				tt = dom.Next()
+				tok = dom.Token()
+				if tt == html.TextToken {
+					book.Runtime = cleanstr(tok.Data)
+				}
+			}
+			continue
+		} else if strings.Contains(href(tok), "/series/") {
+			////////// SERIES and SERIESINDEX
+			book.Series, book.SeriesIndex = xSeries(dom, tt, tok)
+			continue
+		} else if !strings.Contains(aria_label(tok), "DownloadPart ") &&
+			strings.Contains(href(tok), "cds.audible.com/download") {
+			////////// DOWNLOAD URL
+			book.DownloadURL = href(tok)
+			continue
+		} else if href(tok) == "/companion-file/"+book.Slug {
+			////////// DOWNLOAD URL
+			book.CompanionURL = "https://audible.com" + cleanstr(href(tok))
+			continue
+		}
+
+		// We've arrived at the next book
+		if strings.Contains(class(tok), "library-item-divider") ||
+			id(tok) == "adbl-library-content-toast-messaging" {
+			break
+		}
+	}
+	book.FileName = stripstr(book.Title)
+	return book
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -343,5 +677,25 @@ func doScrapeLibrary(client Client, account string) {
 		}
 		fmt.Println("FOR ACCOUNT  ", a.Name)
 		fmt.Println("WITH BYTES   ", a.Bytes)
+
+		ch := make(chan int)
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			var t int
+			for i := range ch {
+				t = i
+				fmt.Printf("\x1b[2k\r\033[1mScraping Page\033[m %d", i)
+			}
+			fmt.Printf("\x1b[2k\r\033[1mScraped Page\033[m %d/%d\n", t, t)
+		}()
+		books, err := a.ScrapeFullLibrary(ch)
+		wg.Wait()
+		unwrap(err)
+
+		for _, b := range books {
+			fmt.Println(b)
+		}
 	}
 }
